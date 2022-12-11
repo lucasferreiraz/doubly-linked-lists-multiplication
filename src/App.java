58f2a8650d1c6be0224dfaea263b89dd78f8125a
Lucas Ferreira <ferreiralucas090@gmail.com>
2022-12-11 04:09:32 -0300

    feat: some I/O treatments

--- src/App.java
+++ src/App.java
@@ -1,5 +1,29 @@
+import java.util.Scanner;
+
+import parses.Parse;
+import structures.DoublyLinkedList;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        Scanner scanner = new Scanner(System.in);
+        Parse parse = new Parse();
+
+        System.out.println("Digite o primeiro número: ");
+        long n1 = scanner.nextLong();
+
+        DoublyLinkedList list1 = parse.parseList(n1);
+        System.out.println(list1);
+
+        System.out.println("\nDigite o segundo número: ");
+        long n2 = scanner.nextLong();
+
+        DoublyLinkedList list2 = parse.parseList(n2);
+        System.out.println(list2);
+
+        System.out.println("O produto é igual a: \n");
+        System.out.println(parse.multiply(n1, n1));
+        
+        
     }
 }
